556716fa5d58e3be0ec76705a048ff73463955d6
Khan-Xu <65407325+Khan-Xu@users.noreply.github.com>
2020-11-09 10:05:37 +0800

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,56 @@
+#-----------------------------------------------------------------------------#
+# Copyright (c) 2020 Institute of High Energy Physics Chinese Academy of 
+#                    Science
+#-----------------------------------------------------------------------------#
+
+__authors__  = "Han Xu - HEPS Hard X-ray Scattering Beamline (B4)"
+__date__     = "Date : 10.07.2020"
+__version__  = "Alpha-2.0"
+
+
+"""
+plot: Plot the result. 
+
+paper: "A Wave Optics Model for the Effect of Partial Coherence on Coherent 
+        Diffractive Imaging"
+        
+fig1. the secondary source. Figure.2 of paper.
+fig2. the intensity at the sample plane. Figure.7a of paper. 
+    
+Functions: none.
+           
+Classes  : none.    
+"""
+
+#-----------------------------------------------------------------------------#
+# libaries
+
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+#-----------------------------------------------------------------------------#
+# plot secondary source
+
+secondary_source = pickle.load(open("secondary_source.pkl", 'rb'))
+
+x = np.linspace(-30*0.5, 30*0.5, 61)
+y = np.linspace(-15*0.5, 15*0.5, 31)
+intensity = secondary_source.intensity
+
+plt.figure(figsize = (8, 4))
+plt.pcolor(x, y, intensity, cmap = 'jet') 
+plt.colorbar()
+
+#-----------------------------------------------------------------------------#
+# plot sample plane
+
+secondary_source = pickle.load(open("sample_plane.pkl", 'rb'))
+
+x = np.linspace(-64*0.1575, 64*0.1575, 129)
+y = np.linspace(-64*0.1575, 64*0.1575, 129)
+intensity = secondary_source.intensity
+
+plt.figure(figsize = (5, 5))
+plt.pcolor(x, y, intensity, cmap = 'jet') 
+plt.colorbar()
